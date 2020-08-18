f5e11e9f4d759fd5b4677f419f7aa1bd8f8e8f18
tech-student-oono <68591022+tech-student-oono@users.noreply.github.com>
2020-08-18 19:16:36 +0900

    誤ったcommit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe '#create' do
-    before do
-      @user = FactoryBot.build(:user)
-    end
 
     it "nameとemail、passwordとpassword_confirmationが存在すれば登録できること" do
       expect(@user).to be_valid
