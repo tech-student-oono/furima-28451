7e326bd7c0ac678814cf5a1da7ef1a022e9236b7
tech-student-oono <68591022+tech-student-oono@users.noreply.github.com>
2020-08-19 12:25:36 +0900

    [確認後修正]バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
+  with_options presence: true do
+    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。"}
+    validates :name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。"}
+    validates :password, format: { with: /\A[a-z0-9]+\z/i, message: "は半角英数で入力してください。"}
+  end
 
 end
