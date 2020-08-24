18784b23df64026eff9e90d177e66cb0e57291b1
tech-student-oono <68591022+tech-student-oono@users.noreply.github.com>
2020-08-24 14:48:55 +0900

    ログイン/ログアウト完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'
-   }
-   devise_scope :user do 
-    post 'users/sign_up/confirm' => 'users/registrations#confirm'
-    post 'users/sign_up/complete' => 'users/registrations#complete' 
-   end
+  devise_for :users
   get 'users/create'
   root to: 'items#index'
   resources :items
