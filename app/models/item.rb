9cb92bc3f789987d2480a005b916d161db59ef40
tech-student-oono <68591022+tech-student-oono@users.noreply.github.com>
2020-08-29 19:29:26 +0900

    バリデーション、テストコード修正
    金額に対するバリデーション追加
    「商品が出品できる」際のテストコード追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,6 @@ class Item < ApplicationRecord
   validates :item_image, :item_name, :item_info, :item_price, :category_id, :status_id, :fee_id, :prefecture_id, :scheduled_id, presence: true
 
   validates :category_id, :status_id, :fee_id, :scheduled_id, numericality: { other_than: 1 }
+
+  validates :item_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 end
