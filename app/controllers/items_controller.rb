e0474bd040398973449e8e9a1300cefd8d1152b1
tech-student-oono <68591022+tech-student-oono@users.noreply.github.com>
2020-08-30 18:07:43 +0900

    出品した順に商品を表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show]
 
   def index
-    @items = Item.all
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
