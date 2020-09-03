deb171dae6e55bccbd48f04819e1c4ba3aca7737
tech-student-oono <68591022+tech-student-oono@users.noreply.github.com>
2020-09-03 16:06:45 +0900

    エラーハンドリング処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,8 +23,11 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    @item.update(item_params)
-    redirect_to item_path(@item)
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
   end
 
   def show
